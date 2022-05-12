549e5a33c7c7b81d509a089b6e412d4c834b0617
Carl <cmuell89@gmail.com>
2022-05-12 09:45:18 -0600

    WIP: Definining the MINLP

---
+++ nodes/optimization.py
@@ -0,0 +1,5 @@
+from cairo_lfd.lfd.optimization import Constraint_1_2
+
+
+if __name__ == '__main__':
+    
--- src/cairo_2d_sim/lfd/optimization.py
+++ src/cairo_2d_sim/lfd/optimization.py
@@ -1,40 +1,35 @@
 from pyomo.environ import *
 import matplotlib.pyplot as plt
 
-# max 			f1 = X1
-# max 			f2 = 3 X1 + 4 X2
-# constraints	X1 <= 20
-#     			X2 <= 40
-# 				5 X1 + 4 X2 <= 200
 
-class BoxConstraint():
+class Constraint_1_2():
     
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
+    def __init__(self, intersection_x, intersection_y, height=1800, width=1000):
+        self.intersection = (intersection_x, intersection_y)
         self.height = height
+        self.width = width
+    
+    def generate_model(self, keyframe_point):
+        model = ConcreteModel()
+
+        model.x = Var(within=NonNegativeReals)
+        model.y = Var(within=NonNegativeReals)
+
+        model.a = Var(within=Binary)
+
+        model.C1 = Constraint(expr = model.x <= self.height)
+        model.C2 = Constraint(expr = model.x >= 0)
+        model.C3 = Constraint(expr = model.y <= self.width)
+        model.C4 = Constraint(expr = model.y <= 0)
+
+        distance_from_intersection = ((model.x - self.intersection[0])**2 + (model.y - self.intersection[1])**2)**0.5
+        distance_from_keyframe_point = ((model.x - keyframe_point[0])**2 + (model.y - keyframe_point[1])**2)**0.5
         
-    def build_constraint(self, model):
-        model.box_constraint1 = Constraint(expr=self.x <= self.width)
-        model.box_constraint = Constraint(expr=self.y <= self.height)
+        model.obj_1 = Objective(expr = distance_from_intersection, sense=minimize)
+        model.obj_2 = Objective(expr = distance_from_keyframe_point, sense=minimize)
 
-# model = ConcreteModel()
-
-# model.X1 = Var(within=NonNegativeReals)
-# model.X2 = Var(within=NonNegativeReals)
-
-# model.C1 = Constraint(expr = model.X1 <= 20)
-# model.C2 = Constraint(expr = model.X2 <= 40)
-# model.C3 = Constraint(expr = 5 * model.X1 + 4 * model.X2 <= 200)
-
-# model.f1 = Var()
-# model.f2 = Var()
-# model.C_f1 = Constraint(expr = model.f1 == model.X1)
-
-# C_f2 = Constraint(expr = model.f2 == 3 * model.X1 + 4 * model.X2)
-# model.O_f1 = Objective(expr = model.f1, sense=maximize)
-# model.O_f2 = Objective(expr = model.f2, sense=maximize)
-
-# solver = SolverFactory('glpk')  #'cplex', 'ipopt'
-# solver.solve(model)
+        return model
+    
+    def solve(self, model):
+        solver = SolverFactory('mindtpy').solve(model, mip_solver='glpk', nlp_solver='ipopt')   #'cplex', 'ipopt'
+        solver.solve(model)
